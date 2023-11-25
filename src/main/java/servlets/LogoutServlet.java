062067c0dee57070890c273b716308fb2167d995
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-24 17:19:06 -0800

    Added LogoutServlet.java class to implement  logic

---
+++ src/main/java/servlets/LogoutServlet.java
@@ -0,0 +1,23 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session.getAttribute("username") != null) {
+            session.setAttribute("username", null);
+            session.invalidate();
+            response.sendRedirect("/login");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
