9d5979757bf39aa83a90551b3e0be7c8f2a74209
ruchamaslekar <ruchamaslekar@gmail.com>
2023-11-24 17:20:44 -0800

    Added ThreadSafeHotelDetails.java class from project 4

---
+++ src/main/java/hotelData/ThreadSafeHotelDetails.java
@@ -0,0 +1,67 @@
+package hotelData;
+
+import com.google.gson.JsonObject;
+
+import java.util.Set;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/** This class is extending HotelDetails class and implementing locks */
+public class ThreadSafeHotelDetails extends HotelDetails {
+    private final ReentrantReadWriteLock lock;
+    public ThreadSafeHotelDetails() {
+        lock = new ReentrantReadWriteLock();
+
+    }
+
+    /** Overridden getHotel() method from HotelDetails class */
+    @Override
+    public Hotel getHotel(String hotelID) {
+        try{
+            lock.readLock().lock();
+            return super.getHotel(hotelID);
+        }
+        finally {
+            lock.readLock().unlock();
+        }
+    }
+    /** Overridden addHotel() method from HotelDetails class */
+    @Override
+    public void addHotel(String hotelId, Hotel hotel) {
+        try{
+            lock.writeLock().lock();
+            super.addHotel(hotelId,hotel);
+        }
+        finally {
+            lock.writeLock().unlock();
+        }
+    }
+
+    /** Overridden getIdForHotel() method from HotelDetails class */
+    public Set<String> getIdForHotel() {
+        return super.getIdForHotel();
+    }
+
+    /** Overridden getHotelInJSONFormat() method from HotelDetails class */
+    @Override
+    public JsonObject getHotelInJSONFormat(String hotelId) {
+        try{
+            lock.readLock().lock();
+            return super.getHotelInJSONFormat(hotelId);
+        }
+        finally {
+            lock.readLock().unlock();
+        }
+    }
+
+    /** Overridden getWeatherDataInJSONFormat() method from HotelDetails class */
+//    @Override
+//    public JsonObject getWeatherDataInJSONFormat(String hotelId) {
+//        try{
+//            lock.readLock().lock();
+//            return super.getWeatherDataInJSONFormat(hotelId);
+//        }
+//        finally {
+//            lock.readLock().unlock();
+//        }
+//    }
+}
---
+++ src/main/java/server/ProgramArgumentParser.java
@@ -0,0 +1,30 @@
+package server;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ProgramArgumentParser {
+    private final Map<String, String> arguments = new HashMap<>();
+
+    /** Method to add key value pair for input parameters in map */
+    public void parseArgs(String[] args) {
+        for (int i = 0; i < args.length; i += 2) {
+            if (i + 1 < args.length) {
+                arguments.put(args[i], args[i + 1]);
+            }
+        }
+    }
+
+    /** Method to get key value pair for input parameters from map */
+    public String getArgument(String argName) {
+        if(argName.equals("-threads")) {
+            if (arguments.get(argName) == null) {
+                return "1";
+            }
+            else {
+                return arguments.get(argName);
+            }
+        }
+        return this.arguments.get(argName);
+    }
+}
