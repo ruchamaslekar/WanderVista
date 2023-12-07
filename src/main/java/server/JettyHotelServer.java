675a31eee20a9451cb23f491122ab280378ebe24
ruchamaslekar <ruchamaslekar@gmail.com>
2023-12-06 18:17:08 -0800

    Added a helper method in JettyHotelServer.java class to put the code that we want to use only once(commented code)

--- src/main/java/server/JettyHotelServer.java
+++ src/main/java/server/JettyHotelServer.java
@@ -1,32 +1,19 @@
 package server;
 
+import reviewData.DirectoryParser;
+import reviewData.Review;
 import servlets.*;
+import hotelData.*;
+
+import java.io.IOException;
+import java.util.List;
 
 public class JettyHotelServer {
 	public static final int PORT = 8080;
 
 	public static void main(String[] args) throws Exception {
 		// FILL IN CODE, and add more classes as needed
-//		ProgramArgumentParser programParser = new ProgramArgumentParser();
-//		programParser.parseArgs(args);
-//		/** Loading data for Hotel */
-//		HotelParser parser = new HotelParser();
-//		HotelDetails hotelDetails = new ThreadSafeHotelDetails();
-//		parser.parseHotelJson(programParser.getArgument("-hotels"),hotelDetails);
-//		List<Hotel> list = parser.parseHotelJson(programParser.getArgument("-hotels"),hotelDetails);
-//
-//		/** Loading data for Reviews */
-//		DirectoryParser directoryParser = new DirectoryParser();
-//		List<List<Review>> reviewList = directoryParser.parseDirectory(programParser.getArgument("-reviews"));
-//
-//		/** Creating and inserting into database */
-//		DatabaseHandler databaseHandler = DatabaseHandler.getInstance();
-//		databaseHandler.createTable();
-//		databaseHandler.createHotelsTable();
-//		databaseHandler.insertIntoHotelsTable(list);
-//		databaseHandler.createReviewsTable();
-//		databaseHandler.insertIntoReviewsTable(reviewList);
-
+//		databaseHelper(args);
 		JettyServer server = new JettyServer();
 		server.addMapping("/login", LoginServlet.class);
 		server.addMapping("/register", RegistrationServlet.class);
@@ -39,5 +26,26 @@ public class JettyHotelServer {
 		server.addMapping("/editReviews", EditReviewsServlet.class);
 		server.addMapping("/logout", LogoutServlet.class);
 		server.start();
+	}
+
+	public static void databaseHelper(String[] args) throws IOException {
+		ProgramArgumentParser programParser = new ProgramArgumentParser();
+		programParser.parseArgs(args);
+		/** Loading data for Hotel */
+		HotelParser parser = new HotelParser();
+		HotelDetails hotelDetails = new ThreadSafeHotelDetails();
+		parser.parseHotelJson(programParser.getArgument("-hotels"),hotelDetails);
+		List<Hotel> list = parser.parseHotelJson(programParser.getArgument("-hotels"),hotelDetails);
+		/** Loading data for Reviews */
+		DirectoryParser directoryParser = new DirectoryParser();
+		List<List<Review>> reviewList = directoryParser.parseDirectory(programParser.getArgument("-reviews"));
+		/** Creating and inserting into database */
+		DatabaseHandler databaseHandler = DatabaseHandler.getInstance();
+		databaseHandler.createTable();
+		databaseHandler.createHotelsTable();
+		databaseHandler.insertIntoHotelsTable(list);
+		databaseHandler.createReviewsTable();
+		databaseHandler.insertIntoReviewsTable(reviewList);
+		databaseHandler.createLoginTable();
 	}
 }
