dcbf7982fef649e23f62046a9779b16bc7b39523
ruchamaslekar <ruchamaslekar@gmail.com>
2023-12-13 17:11:52 -0500

    Added HotelMapServlet.java class to add logic to get location of a hotel in map

---
+++ src/main/java/servlets/HotelMapServlet.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import thyemeleaf.ThymeLeafConfig;
+import thyemeleaf.ThymeLeafRenderer;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HotelMapServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        double latitude = Double.parseDouble(request.getParameter("lat"));
+        double longitude = Double.parseDouble(request.getParameter("lon"));
+        PrintWriter out = response.getWriter();
+        request.setAttribute("latitude", latitude);
+        request.setAttribute("longitude", longitude);
+        ThymeLeafConfig thymeleafConfig = new ThymeLeafConfig();
+        ThymeLeafRenderer thymeleafRenderer = new ThymeLeafRenderer(thymeleafConfig.templateEngine());
+        thymeleafRenderer.render("hotel-map", out);
+    }
+}
